d6fe344c887c355c8c19f6d546c7b4d625ec5ee8
MomoHunter <moris_92@web.de>
2019-09-25 21:08:40 +0200

    split files

---
+++ script.js
@@ -0,0 +1,9 @@
+const vocabs = [
+  { deutsch: "ja", romaji: "hai", kana: "はい" },
+  { deutsch: "nein", romaji: "iie", kana: "いいえ" },
+  { deutsch: "bitte (beim Anbieten)", romaji: "douzo", kana: "どうぞ" }
+];
+
+var start = new Vue({
+  el: '#mainMenu'
+});
