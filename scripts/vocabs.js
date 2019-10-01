c3068497c1c2bb0275dca1bd902de056536f2403
MomoHunter <moris_92@web.de>
2019-10-01 09:20:54 +0200

    added difficulty description to vocabs

--- scripts/vocabs.js
+++ scripts/vocabs.js
@@ -1,3 +1,9 @@
+/**
+ * the difficulty is dependent on the used kana and the length of the words:
+ * difficulty 1 (easy): only hiragana and 5 signs at max
+ * difficulty 2 (medium): hiragana and katakana and 10 signs at max
+ * difficulty 3 (hard): all kana and unlimited signs
+ */
 const vocabs = [
   { german: "ja", romaji: "hai", kana: "はい", difficulty: "1" },
   { german: "nein", romaji: "iie", kana: "いいえ", difficulty: "1" },
