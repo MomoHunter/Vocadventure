d83ddd8b17c860745a6d26e8dc8e78a267269a77
MomoHunter <moris_92@web.de>
2019-10-14 20:48:06 +0200

    added signs and 3 new vocabs

--- scripts/vocabs.js
+++ scripts/vocabs.js
@@ -5,7 +5,11 @@
  * difficulty 3 (hard): all kana and unlimited signs
  */
 const vocabs = [
-  { "Deutsch": "ja", "English": "yes", romaji: "hai", kana: "はい", difficulty: "1" },
-  { "Deutsch": "nein", "English": "no", romaji: "iie", kana: "いいえ", difficulty: "1" },
-  { "Deutsch": "bitte (beim Anbieten)", "English": "please (offering)", romaji: "douzo", kana: "どうぞ", difficulty: "1" }
+  { "Deutsch": "Ja", "English": "yes", romaji: "hai", kana: "はい", difficulty: "1" },
+  { "Deutsch": "Nein", "English": "no", romaji: "iie", kana: "いいえ", difficulty: "1" },
+  { "Deutsch": "Bitte (beim Anbieten)", "English": "please (offering)", romaji: "douzo", kana: "どうぞ", difficulty: "1" },
+  { "Deutsch": "Danke", "English": "Thanks", romaji: "doumo", kana: "どうも", difficulty: "1" },
+  { "Deutsch": "Heute", "English": "Today", romaji: "kyou", kana: "きょう", difficulty: "1" },
+  { "Deutsch": "Morgen", "English": "Tomorrow", romaji: "ashita", kana: "あした", difficulty: "1" },
+  { "Deutsch": "Übermorgen", "English": "The day after tomorrow", romaji: "asatte", kana: "あさって", difficulty: "1" }
 ];
