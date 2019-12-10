068e93118d85141286f8b24b4b770f304b89855d
MomoHunter <moris_92@web.de>
2019-12-10 20:50:10 +0100

    added some vocabs

--- scripts/vocabs.js
+++ scripts/vocabs.js
@@ -6,7 +6,13 @@
  */
 const vocabs = [
   { "Deutsch": "Ja", "English": "yes", romaji: "hai", kana: "はい", difficulty: "1" },
+  { "Deutsch": "m-hm", "English": "u-huh", romaji: "un", kana: "うん", difficulty: "1" },
+  { "Deutsch": "Ja, genau, stimmt", "English": "yes, exactly, correct", romaji: "sou (sou sou)", kana: "そう　(そう　そう)", difficulty: "2" },
   { "Deutsch": "Nein", "English": "no", romaji: "iie", kana: "いいえ", difficulty: "1" },
+  { "Deutsch": "Nein (emotionaler)", "English": "no (more emotional)", romaji: "iya", kana: "いや", difficulty: "1" },
+  { "Deutsch": "Nö", "English": "Nah, nope", romaji: "uun", kana: "いいえ", difficulty: "1" },
+  { "Deutsch": "Äh", "English": "um", romaji: "ano", kana: "あの", difficulty: "1" },
+  { "Deutsch": "Hm...", "Englisch": "Hm...", romaji: "saa", kana: "さあ", difficulty: "1"},
   { "Deutsch": "Bitte (beim Anbieten)", "English": "please (offering)", romaji: "douzo", kana: "どうぞ", difficulty: "1" },
   { "Deutsch": "Danke", "English": "Thanks", romaji: "doumo", kana: "どうも", difficulty: "1" },
   { "Deutsch": "Heute", "English": "Today", romaji: "kyou", kana: "今日", difficulty: "3" },
