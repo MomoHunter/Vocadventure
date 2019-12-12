735f1da27a56638e75fcfc8e5d0c24c250c3d47c
MomoHunter <moris_92@web.de>
2019-12-12 17:05:40 +0100

    fixed a word

--- scripts/vocabs.js
+++ scripts/vocabs.js
@@ -10,7 +10,7 @@ const vocabs = [
   { "Deutsch": "Ja, genau, stimmt", "English": "yes, exactly, correct", romaji: "sou (sou sou)", kana: "そう　(そう　そう)", difficulty: "2" },
   { "Deutsch": "Nein", "English": "no", romaji: "iie", kana: "いいえ", difficulty: "1" },
   { "Deutsch": "Nein (emotionaler)", "English": "no (more emotional)", romaji: "iya", kana: "いや", difficulty: "1" },
-  { "Deutsch": "Nö", "English": "Nah, nope", romaji: "uun", kana: "いいえ", difficulty: "1" },
+  { "Deutsch": "Nö", "English": "Nah, nope", romaji: "uun", kana: "ううん", difficulty: "1" },
   { "Deutsch": "Äh", "English": "um", romaji: "ano", kana: "あの", difficulty: "1" },
   { "Deutsch": "Hm...", "Englisch": "Hm...", romaji: "saa", kana: "さあ", difficulty: "1"},
   { "Deutsch": "Bitte (beim Anbieten)", "English": "please (offering)", romaji: "douzo", kana: "どうぞ", difficulty: "1" },
