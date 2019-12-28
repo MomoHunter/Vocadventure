890ee9c6b40f95c16a28b1c54fd1dc61c78d3cd4
MomoHunter <moris_92@web.de>
2019-12-28 22:52:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <TheStatus :status="status"></TheStatus>
+  </div>
+</template>
+
+<script>
+import TheStatus from './components/TheStatus'
+
+export default {
+  name: 'app',
+  components: {
+    TheStatus
+  },
+  data: function () {
+    return {
+      status: [
+        { id: 'points', count: 0 },
+        { id: 'steps', count: 0 },
+        { id: 'coins', count: 0 }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+@import "~bulma/css/bulma.min.css";
+</style>
---
+++ src/components/TheStatus.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="level is-mobile">
+    <div class="level-item has-text-centered" v-for="statusItem in status" :key="statusItem.id">
+      <div>
+        <p class="heading">{{ getText(statusItem.id) }}</p>
+        <p class="title">{{ statusItem.count }}</p>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import Texts from './../data/Texts'
+
+export default {
+  name: 'TheStatus',
+  props: ['status'],
+  methods: {
+    getText: function (id) {
+      return Texts['Deutsch'][id]
+    }
+  }
+}
+</script>
