9b6f62d953e92cc8c5185676a4a05c0b2eb435b9
MomoHunter <moris_92@web.de>
2020-06-07 20:03:09 +0200

    pimped deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,21 +3,24 @@
 # abort on errors
 set -e
 
+if [ "$1" == "sass" ]
+then
 # build scss files
-sass sass/darkly.bw.scss public/css/darkly.bw.min.css --style=compressed
-sass sass/cerulean.bw.scss public/css/cerulean.bw.min.css --style=compressed
-sass sass/slate.bw.scss public/css/slate.bw.min.css --style=compressed
-sass sass/cosmo.bw.scss public/css/cosmo.bw.min.css --style=compressed
-sass sass/cyborg.bw.scss public/css/cyborg.bw.min.css --style=compressed
-sass sass/flatly.bw.scss public/css/flatly.bw.min.css --style=compressed
-sass sass/lumen.bw.scss public/css/lumen.bw.min.css --style=compressed
-sass sass/darkLumen.scss public/css/darkLumen.min.css --style=compressed
-sass sass/lux.bw.scss public/css/lux.bw.min.css --style=compressed
-sass sass/materia.bw.scss public/css/materia.bw.min.css --style=compressed
-sass sass/nuclear.bw.scss public/css/nuclear.bw.min.css --style=compressed
-sass sass/solar.bw.scss public/css/solar.bw.min.css --style=compressed
-sass sass/spacelab.bw.scss public/css/spacelab.bw.min.css --style=compressed
-sass sass/superhero.bw.scss public/css/superhero.bw.min.css --style=compressed
+  sass sass/darkly.bw.scss public/css/darkly.bw.min.css --style=compressed
+  sass sass/cerulean.bw.scss public/css/cerulean.bw.min.css --style=compressed
+  sass sass/slate.bw.scss public/css/slate.bw.min.css --style=compressed
+  sass sass/cosmo.bw.scss public/css/cosmo.bw.min.css --style=compressed
+  sass sass/cyborg.bw.scss public/css/cyborg.bw.min.css --style=compressed
+  sass sass/flatly.bw.scss public/css/flatly.bw.min.css --style=compressed
+  sass sass/lumen.bw.scss public/css/lumen.bw.min.css --style=compressed
+  sass sass/darkLumen.scss public/css/darkLumen.min.css --style=compressed
+  sass sass/lux.bw.scss public/css/lux.bw.min.css --style=compressed
+  sass sass/materia.bw.scss public/css/materia.bw.min.css --style=compressed
+  sass sass/nuclear.bw.scss public/css/nuclear.bw.min.css --style=compressed
+  sass sass/solar.bw.scss public/css/solar.bw.min.css --style=compressed
+  sass sass/spacelab.bw.scss public/css/spacelab.bw.min.css --style=compressed
+  sass sass/superhero.bw.scss public/css/superhero.bw.min.css --style=compressed
+fi
 
 # build
 npm run build
@@ -45,4 +48,9 @@ cp -r dist tmp/Vocadventure
 
 cd tmp
 
-livereload
+if [ "$1" == "livereload" ]
+then
+  livereload
+else
+  exit
+fi
