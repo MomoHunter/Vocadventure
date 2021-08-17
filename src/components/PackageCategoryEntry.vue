498ece2d50a0e6301907d1432ca6072abbceaa46
MomoHunter <moris_92@web.de>
2021-08-17 22:22:41 +0200

    added numbers to words in a package

--- src/components/PackageCategoryEntry.vue
+++ src/components/PackageCategoryEntry.vue
@@ -2,7 +2,7 @@
   <div class="package-category-entry flex-column" :class="getSizeClass('general')">
     <div class="flex-row">
       <ButtonIcon class="single-1" :icon="expandIcon" color="action" @click="$emit('expand')" />
-      <div class="details flex-grow info">
+      <div class="title flex-grow info">
         {{ category[$store.state.lang] }}
       </div>
       <ButtonIcon class="single-1" icon="pen" color="green" @click="$emit('edit', { type: 'category' })" />
@@ -11,8 +11,13 @@
     <ButtonBasic v-show="expanded" class="single-1" icon="plus" color="green" text="packagesEditCategoriesButton3"
                  @click="$emit('new')" />
     <div v-show="expanded" class="flex-row" v-for="(word, index) in category.words" :key="index">
-      <div class="details flex-grow">
-        {{ word[$store.state.lang] }}
+      <div class="details flex-grow flex-row">
+        <div class="index">
+          {{ index + 1 }}
+        </div>
+        <div class="word">
+          {{ word[$store.state.lang] }}
+        </div>
       </div>
       <ButtonIcon class="single-1" icon="pen" color="green" @click="$emit('edit', { type: 'word', index: index })" />
       <ButtonIcon class="single-1" icon="trash" color="red" @click="$emit('delete', { type: 'word', index: index })" />
