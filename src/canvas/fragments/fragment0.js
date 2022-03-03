99ce6a6babc8cb6c8acdd5be0513fb6ff3f4c4ae
MomoHunter <moris_92@web.de>
2022-03-03 15:26:10 +0100

    added new scene

--- src/canvas/fragments/fragment0.js
+++ src/canvas/fragments/fragment0.js
@@ -16,11 +16,12 @@ export const updateCalls = [
   p13Update,
   p14Update,
   p15Update,
-  chromeParkUpdate,
-  constructionUpdate,
-  diagramUpdate,
-  entryChromeleonUpdate,
-  p20Update
+  p16Update,
+  p17Update,
+  p18Update,
+  p19Update,
+  p20Update,
+  p21Update
 ]
 
 export const drawCalls = [
@@ -39,11 +40,12 @@ export const drawCalls = [
   p13Draw,
   p14Draw,
   p14Draw,
-  chromeParkDraw,
-  constructionDraw,
-  diagramDraw,
-  entryChromeleonDraw,
-  p20Draw
+  p16Draw,
+  p17Draw,
+  p18Draw,
+  p19Draw,
+  p20Draw,
+  p21Draw
 ]
 
 let data = {
@@ -135,40 +137,59 @@ let data = {
       logoRotation: 0
     }
   },
-  chromander: {
-    pause: 0,
-    opacity: {
-      nowNew: 0,
-      headline: 0,
-      logo: 0,
-      bottomText: 0,
-      eye: 0
-    },
-    logoRotation: 0
-  },
-  chromePark: {
-    pause: 0,
-    shift: 0,
-    opacity: 0
-  },
-  construction: {
-    roller: {
-      shift: 150,
-      startFrame: 0,
-      pause: 0
-    },
-    crane: {
-      rope: 0,
+  p16: {
+    dyn: {
+      pause: 0,
       shift: 0,
-      pause: 0
-    },
-    tree: {
-      x: 0,
-      y: 0
+      opacity: 0
     }
   },
-  diagram: {
-    factor: 0
+  p17: {
+    con: {
+      crane: {
+        speed: 0.3
+      },
+      tree: {
+        start: {
+          x: 100,
+          y: 60
+        },
+        end: {
+          x: 155,
+          y: 80
+        }
+      }
+    },
+    dyn: {
+      roller: {
+        startFrame: 0,
+        shift: 150,
+        pause: 0
+      },
+      crane: {
+        rope: 0,
+        shift: 0,
+        pause: 0
+      },
+      tree: {
+        x: 0,
+        y: 0
+      }
+    }
+  },
+  p18: {
+    con: {
+      factorSpeed: 0.002
+    },
+    dyn: {
+      factor: 0
+    }
+  },
+  p20: {
+    dyn: {
+      people: [],
+      newPeopleCountdown: 60
+    }
   }
 }
 
@@ -372,7 +393,7 @@ function p4Draw () {
       halfWidth * (1 - videoZoom), halfHeight * (1 - videoZoom) + data.p4.dyn.shift.y, videoZoom,
       'story_f0_video_start_background', cD
     )
-    if (data.pause < 30) {
+    if (data.p4.dyn.pause < 30) {
       Helper.drawCanvasSmallImage(
         halfWidth - 50 * videoZoom, halfHeight - 50 * videoZoom + data.p4.dyn.shift.y, videoZoom,
         'story_f0_video_start_button', cD
@@ -775,135 +796,151 @@ function p15Update () {
   }
 }
 
-function chromeParkUpdate () {
-  if (data.chromePark.pause < 60) {
-    data.chromePark.pause += 1
-  } else if (data.chromePark.shift < 150) {
-    data.chromePark.shift += 2
-  } else if (data.chromePark.opacity < 1) {
-    data.chromePark.opacity += 0.01
-    if (data.chromePark.opacity > 1) {
-      data.chromePark.opacity = 1
+/**
+ * splits park in two and fade one out
+ */
+function p16Update () {
+  if (data.p16.dyn.pause < 60) {
+    data.p16.dyn.pause += 1
+  } else if (data.p16.dyn.shift < 150) {
+    data.p16.dyn.shift += 2
+  } else if (data.p16.dyn.opacity < 1) {
+    data.p16.dyn.opacity += 0.01
+    if (data.p16.dyn.opacity > 1) {
+      data.p16.dyn.opacity = 1
     }
   }
 }
 
-function constructionUpdate () {
-  let frames = this.$store.state.canvasDict.frameNo - data.construction.roller.startFrame
-
-  if (frames / 200 < Math.PI * 2) {
-    data.construction.roller.shift = Math.cos(frames / 200) * 150
-  } else if (data.construction.roller.pause < 240) {
-    data.construction.roller.pause += 1
-  } else {
-    data.construction.roller.pause = 0
-    data.construction.roller.startFrame = this.$store.state.canvasDict.frameNo
-  }
-
-  if (data.construction.crane.shift < 55 && data.construction.crane.pause === 0) {
-    data.construction.crane.shift += 0.3
-    if (data.construction.crane.shift > 55) {
-      data.construction.crane.shift = 55
-    }
-    data.construction.tree.x = data.construction.crane.shift
-  } else if (data.construction.crane.rope < 20 && data.construction.crane.pause === 0) {
-    data.construction.crane.rope += 0.3
-    if (data.construction.crane.rope > 20) {
-      data.construction.crane.rope = 20
-    }
-    data.construction.tree.y = data.construction.crane.rope
-  } else if (data.construction.crane.pause < 60) {
-    data.construction.crane.pause += 1
-  } else if (data.construction.crane.rope > 0 && data.construction.crane.pause === 60) {
-    data.construction.crane.rope -= 0.3
-    if (data.construction.crane.rope < 0) {
-      data.construction.crane.rope = 0
-    }
-  } else if (data.construction.crane.shift > 0 && data.construction.crane.pause === 60) {
-    data.construction.crane.shift -= 0.3
-    if (data.construction.crane.shift < 0) {
-      data.construction.crane.shift = 0
-    }
-  } else if (data.construction.crane.pause < 180) {
-    data.construction.crane.pause += 1
-  } else if (data.construction.crane.shift < 55 && data.construction.crane.pause === 180) {
-    data.construction.crane.shift += 0.3
-    if (data.construction.crane.shift > 55) {
-      data.construction.crane.shift = 55
-    }
-  } else if (data.construction.crane.rope < 20 && data.construction.crane.pause === 180) {
-    data.construction.crane.rope += 0.3
-    if (data.construction.crane.rope > 20) {
-      data.construction.crane.rope = 20
-    }
-  } else if (data.construction.crane.pause < 240) {
-    data.construction.crane.pause += 1
-  } else if (data.construction.crane.rope > 0 && data.construction.crane.pause === 240) {
-    data.construction.crane.rope -= 0.3
-    if (data.construction.crane.rope < 0) {
-      data.construction.crane.rope = 0
-    }
-    data.construction.tree.y = data.construction.crane.rope
-  } else if (data.construction.crane.shift > 0 && data.construction.crane.pause === 240) {
-    data.construction.crane.shift -= 0.3
-    if (data.construction.crane.shift < 0) {
-      data.construction.crane.shift = 0
-    }
-    data.construction.tree.x = data.construction.crane.shift
-  } else if (data.construction.crane.pause < 300) {
-    data.construction.crane.pause += 1
-    if (data.construction.crane.pause === 300) {
-      data.construction.crane.pause = 0
-    }
-  }
-}
-
-function diagramUpdate () {
-  if (data.diagram.factor < 1) {
-    data.diagram.factor += 0.002
-    if (data.diagram.factor > 1) {
-      data.diagram.factor = 1
-    }
-  }
-}
-
-function entryChromeleonUpdate () {}
-
-function p20Update () {}
-
-function chromeParkDraw () {
+/**
+ * draws two parks, one will be faded out and a grey one
+ */
+function p16Draw () {
   const cD = this.$store.state.canvasDict
 
   Helper.drawCanvasImage(
-    150 + data.chromePark.shift, 0, 'story_f0_park_chrome', cD, 12, 0, 0, 0, data.chromePark.opacity
+    150 + data.p16.dyn.shift, 0, 'story_f0_park_chrome', cD, 12, 0, 0, 0, data.p16.dyn.opacity
   )
-  Helper.drawCanvasImage(150 + data.chromePark.shift, 0, 'story_f0_park', cD, 12, 0, 0, 0, 1 - data.chromePark.opacity)
-  Helper.drawCanvasImage(150 - data.chromePark.shift, 0, 'story_f0_park', cD)
+  Helper.drawCanvasImage(150 + data.p16.dyn.shift, 0, 'story_f0_park', cD, 12, 0, 0, 0, 1 - data.p16.dyn.opacity)
+  Helper.drawCanvasImage(150 - data.p16.dyn.shift, 0, 'story_f0_park', cD)
 
-  if (data.chromePark.shift === 150) {
+  if (data.p16.dyn.shift === 150) {
     Helper.drawCanvasLine(300, 0, 'standard', cD.context, 300, 300)
   }
 }
 
-function constructionDraw () {
+/**
+ * updates the crane and the roller on the construction site
+ */
+function p17Update () {
+  let frames = this.$store.state.canvasDict.frameNo - data.p17.dyn.roller.startFrame
+  let xDist = data.p17.con.tree.end.x - data.p17.con.tree.start.x
+  let yDist = data.p17.con.tree.end.y - data.p17.con.tree.start.y
+
+  if (frames / 200 < Math.PI * 2) {
+    data.p17.dyn.roller.shift = Math.cos(frames / 200) * 150
+  } else if (data.p17.dyn.roller.pause < 240) {
+    data.p17.dyn.roller.pause += 1
+  } else {
+    data.p17.dyn.roller.pause = 0
+    data.p17.dyn.roller.startFrame = this.$store.state.canvasDict.frameNo
+  }
+
+  if (data.p17.dyn.crane.shift < xDist && data.p17.dyn.crane.pause === 0) {
+    data.p17.dyn.crane.shift += data.p17.con.crane.speed
+    if (data.p17.dyn.crane.shift > xDist) {
+      data.p17.dyn.crane.shift = xDist
+    }
+    data.p17.dyn.tree.x = data.p17.dyn.crane.shift
+  } else if (data.p17.dyn.crane.rope < yDist && data.p17.dyn.crane.pause === 0) {
+    data.p17.dyn.crane.rope += data.p17.con.crane.speed
+    if (data.p17.dyn.crane.rope > yDist) {
+      data.p17.dyn.crane.rope = yDist
+    }
+    data.p17.dyn.tree.y = data.p17.dyn.crane.rope
+  } else if (data.p17.dyn.crane.pause < 60) {
+    data.p17.dyn.crane.pause += 1
+  } else if (data.p17.dyn.crane.rope > 0 && data.p17.dyn.crane.pause === 60) {
+    data.p17.dyn.crane.rope -= data.p17.con.crane.speed
+    if (data.p17.dyn.crane.rope < 0) {
+      data.p17.dyn.crane.rope = 0
+    }
+  } else if (data.p17.dyn.crane.shift > 0 && data.p17.dyn.crane.pause === 60) {
+    data.p17.dyn.crane.shift -= data.p17.con.crane.speed
+    if (data.p17.dyn.crane.shift < 0) {
+      data.p17.dyn.crane.shift = 0
+    }
+  } else if (data.p17.dyn.crane.pause < 180) {
+    data.p17.dyn.crane.pause += 1
+  } else if (data.p17.dyn.crane.shift < xDist && data.p17.dyn.crane.pause === 180) {
+    data.p17.dyn.crane.shift += data.p17.con.crane.speed
+    if (data.p17.dyn.crane.shift > xDist) {
+      data.p17.dyn.crane.shift = xDist
+    }
+  } else if (data.p17.dyn.crane.rope < yDist && data.p17.dyn.crane.pause === 180) {
+    data.p17.dyn.crane.rope += data.p17.con.crane.speed
+    if (data.p17.dyn.crane.rope > yDist) {
+      data.p17.dyn.crane.rope = yDist
+    }
+  } else if (data.p17.dyn.crane.pause < 240) {
+    data.p17.dyn.crane.pause += 1
+  } else if (data.p17.dyn.crane.rope > 0 && data.p17.dyn.crane.pause === 240) {
+    data.p17.dyn.crane.rope -= data.p17.con.crane.speed
+    if (data.p17.dyn.crane.rope < 0) {
+      data.p17.dyn.crane.rope = 0
+    }
+    data.p17.dyn.tree.y = data.p17.dyn.crane.rope
+  } else if (data.p17.dyn.crane.shift > 0 && data.p17.dyn.crane.pause === 240) {
+    data.p17.dyn.crane.shift -= data.p17.con.crane.speed
+    if (data.p17.dyn.crane.shift < 0) {
+      data.p17.dyn.crane.shift = 0
+    }
+    data.p17.dyn.tree.x = data.p17.dyn.crane.shift
+  } else if (data.p17.dyn.crane.pause < 300) {
+    data.p17.dyn.crane.pause += 1
+    if (data.p17.dyn.crane.pause === 300) {
+      data.p17.dyn.crane.pause = 0
+    }
+  }
+}
+
+/**
+ * draws the construction site with a crane, a tree and a roller
+ */
+function p17Draw () {
   const cD = this.$store.state.canvasDict
 
   Helper.drawCanvasImage(0, 0, 'story_f0_construction_site', cD)
   Helper.drawCanvasImage(
-    0 + data.construction.roller.shift, 150 + Math.sin(this.$store.state.canvasDict.frameNo) * 0.6,
+    0 + data.p17.dyn.roller.shift, 150 + Math.sin(this.$store.state.canvasDict.frameNo) * 0.6,
     'story_f0_construction_site_roller', cD
   )
   Helper.drawCanvasLine(
-    123 + data.construction.crane.shift, 40, 'thick', cD.context,
-    123 + data.construction.crane.shift, 70 + data.construction.crane.rope
+    123 + data.p17.dyn.crane.shift, 40, 'thick', cD.context,
+    123 + data.p17.dyn.crane.shift, 70 + data.p17.dyn.crane.rope
   )
   Helper.drawCanvasImage(0, 20, 'story_f0_construction_site_crane', cD)
   Helper.drawCanvasImage(
-    100 + data.construction.tree.x, 60 + data.construction.tree.y, 'story_f0_construction_site_tree', cD
-  ) // start: 100-60; end: 155-80
+    100 + data.p17.dyn.tree.x, 60 + data.p17.dyn.tree.y, 'story_f0_construction_site_tree', cD
+  )
 }
 
-function diagramDraw () {
+/**
+ * updates the size of the diagram bars
+ */
+function p18Update () {
+  if (data.p18.dyn.factor < 1) {
+    data.p18.dyn.factor += data.p18.con.factorSpeed
+    if (data.p18.dyn.factor > 1) {
+      data.p18.dyn.factor = 1
+    }
+  }
+}
+
+/**
+ * draws the diagram of the difference between inside explorer and woogle chromander
+ */
+function p18Draw () {
   const cD = this.$store.state.canvasDict
 
   let logo1Data = Helper.getSpriteData('story_f0_inside_explorer', cD)
@@ -911,20 +948,28 @@ function diagramDraw () {
 
   Helper.drawCanvasImage(0, 0, 'story_f0_video_start_background', cD)
   Helper.drawCanvasImage(
-    150 - logo1Data.spriteWidth / 2, 60 * data.diagram.factor + 75, logo1Data.key, cD
+    150 - logo1Data.spriteWidth / 2, 60 * data.p18.dyn.factor + 75, logo1Data.key, cD
   )
   Helper.drawCanvasRect(
-    100, 180 + 60 * data.diagram.factor, 100, 60 + 60 * (1 - data.diagram.factor), 'storyF0P18InsideExplorer', cD.context
+    100, 180 + 60 * data.p18.dyn.factor, 100, 60 + 60 * (1 - data.p18.dyn.factor), 'storyF0P18InsideExplorer', cD.context
   )
   Helper.drawCanvasImage(
-    450 - logo2Data.spriteWidth / 2, 195 * (1 - data.diagram.factor), logo2Data.key, cD
+    450 - logo2Data.spriteWidth / 2, 195 * (1 - data.p18.dyn.factor), logo2Data.key, cD
   )
   Helper.drawCanvasRect(
-    400, 105 + 195 * (1 - data.diagram.factor), 100, 195 * data.diagram.factor, 'storyF0P18Chromander', cD.context
+    400, 105 + 195 * (1 - data.p18.dyn.factor), 100, 195 * data.p18.dyn.factor, 'storyF0P18Chromander', cD.context
   )
 }
 
-function entryChromeleonDraw () {
+/**
+ * updates nothing
+ */
+function p19Update () {}
+
+/**
+ * draws chromeleon at the entrance of the chrome park
+ */
+function p19Draw () {
   const cD = this.$store.state.canvasDict
 
   Helper.drawCanvasImage(0, 0, 'story_f0_entry_chromeleon', cD)
@@ -933,8 +978,76 @@ function entryChromeleonDraw () {
   )
 }
 
+/**
+ * adds people that look at the picture and moves them
+ */
+function p20Update () {
+  if (data.p20.dyn.newPeopleCountdown > 0) {
+    data.p20.dyn.newPeopleCountdown -= 1
+  } else {
+    var ltr = Math.random() > 0.5
+    data.p20.dyn.people.push({
+      spriteKey: 'story_f0_weird_pic_man',
+      ltr: ltr,
+      x: ltr ? -100 : 610,
+      y: Math.floor(Math.random() * 20) + 280,
+      speed: (Math.random() * 3) + 1
+    })
+    data.p20.dyn.people.sort((a, b) => {
+      if (a.y < b.y) {
+        return -1
+      } else if (a.y > b.y) {
+        return 1
+      }
+      return 0
+    })
+    data.p20.dyn.newPeopleCountdown = Math.floor(180 * Math.random()) + 60
+  }
+
+  let deletePeople = []
+  for (let p of data.p20.dyn.people) {
+    if (p.ltr) {
+      p.x += p.speed
+      if (p.x > this.canvasWidth + 10) {
+        deletePeople.push(p)
+      }
+    } else {
+      p.x -= p.speed
+      if (p.x < -100) {
+        deletePeople.push(p)
+      }
+    }
+  }
+
+  data.p20.dyn.people = data.p20.dyn.people.filter(people => !deletePeople.includes(people))
+}
+
+/**
+ * draws the strange picture that chromeleon drew
+ */
 function p20Draw () {
   const cD = this.$store.state.canvasDict
+  const spriteData = Helper.getSpriteData('story_f0_weird_pic_man', cD)
 
   Helper.drawCanvasImage(0, 0, 'story_f0_picture', cD)
+
+  for (let p of data.p20.dyn.people) {
+    Helper.drawCanvasImage(p.x, p.y - spriteData.spriteHeight, spriteData.key, cD)
+  }
+}
+
+/**
+ *
+ */
+function p21Update () {
+
+}
+
+/**
+ * draws the villa with chromeleon and the rich guy
+ */
+function p21Draw () {
+  const cD = this.$store.state.canvasDict
+
+  Helper.drawCanvasImage(0, 0, 'story_f0_house_of_rich', cD)
 }
